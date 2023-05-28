7b8024a71e7a92ea1ccfb5a1202b6ffc24ce3bfd
jonathanforhan <jonathan.forhan@gmail.com>
2023-05-28 06:13:37 +0000

    first commit

---
+++ build.rs
@@ -0,0 +1,33 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // Tell cargo to look for shared libraries in the specified directory
+    println!("cargo:rustc-link-search=/usr/include/wayland-server.h");
+
+    // Tell cargo to tell rustc to link the system wayland-server shared library
+    println!("cargo:rustc-link-lib=wayland-server");
+
+    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .allowlist_function("wl.*")
+        .allowlist_type("wl.*")
+        .allowlist_var("wl.*")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ src/bin/example.rs
@@ -0,0 +1,23 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+use std::ffi::*;
+
+extern crate owl;
+
+fn main() {
+    let display: *mut wl_display = unsafe { wl_display_create() };
+
+    let socket: *const c_char = unsafe { wl_display_add_socket_auto(display) };
+
+    let msg: &CStr = unsafe { CStr::from_ptr(socket) };
+    let msg = msg.to_str().unwrap();
+
+    println!("Running Wayland display on {msg}");
+
+    unsafe { wl_display_run(display) };
+    unsafe { wl_display_destroy(display) };
+}
